baf504c9e6c197c9577b841296976a02ea679e97
eslammo373 <ee.eslam344@gmail.com>
2024-05-17 05:14:30 +0300

    first commit

---
+++ custom.js
@@ -0,0 +1,43 @@
+let loader = document.getElementById("preloader");
+
+function   autoplay(){
+      window.addEventListener("load", function(){
+        loader.style.display = 'none';
+      });
+
+}
+
+
+
+autoplay();
+
+
+// writter effect
+
+var typed = new Typed('.animate', {
+  strings: [
+      'البشمهندسة رانا محسن',
+      'البشمهندسة رانا محسن',
+      'الدكتورة المنتظرة'
+    
+
+     
+  ],
+  typeSpeed: 40,
+  backSpeed: 40,
+  loop: true
+});  
+window.onload = function() {
+    
+};
+
+document
+
+  window.addEventListener("click", event => {
+    const audio = document.querySelector("audio");
+    audio.volume = 0.2;
+    audio.play();
+  });
+
+
+
